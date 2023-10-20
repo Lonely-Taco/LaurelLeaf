3e5fb9eec39bdc368b907c92b898a7b3eb9a3329
Ramon Gonzalez <47434636+Lonely-Taco@users.noreply.github.com>
2023-10-20 17:40:16 +0200

    pdf convertion.

--- README.md
+++ README.md
@@ -4,42 +4,63 @@
 
 This Python program allows you to process HTML files within a selected folder, including:
 
-- Removing `<script>` tags from the header.
-- Removing `<nav></nav>` tags from the HTML content.
-- Removing a user-specified string from `<link>` and `<img>` tags.
-- Copying files from subfolders to a central `_files` folder in the root directory.
-- Optionally deleting subfolders after a successful copy.
+- **Clean HTML Content**: The program can remove various elements from the HTML content such as scripts, links, meta tags, iframes, navigation tags, and more.
+
+- **Filter URLs**: You can remove URLs from anchor tags in HTML files. Optionally, you can filter out GitHub repository links.
+
+- **Convert to PDF**: The program provides an option to convert the cleaned HTML content to PDF using the wkhtmltopdf library.
+
 
 ## Prerequisites
 
-- Python 3.x is required to run this program. If you don't have Python installed, you can download it from [python.org](https://www.python.org/downloads/).
+Before using this program, make sure you have the following prerequisites:
 
-## Installation
+- Python 3.x: The program is written in Python and requires a Python interpreter.
 
-1. Install the required Python libraries using pip:
+- Tkinter: Tkinter is used for creating the graphical user interface. It is typically included with Python, so you might not need to install it separately.
+
+- wkhtmltopdf: To convert HTML content to PDF, you need to have the wkhtmltopdf library installed. Make sure it's available in your system's PATH or specify the path in the program.
+
+- BeautifulSoup4: You can install BeautifulSoup4 using pip:
+
+  ```bash
+   python3 -m pip install beautifulsoup4
+  ```
+
+   ```sh
+   pip install pdfkit
+   ```
+
+## Run the program:
 
 ```bash
-python3 -m pip install beautifulsoup4
-```
-
-2. Run the program:
-
-```bash
-python3 your_program.py
+python3 app.py
 ```
 
 
-## Usage
 
-1. Run the program and select a folder from which you want to process HTML files.
+## How to Use
 
-2. The program will perform the following actions:
+1. **Choose a Folder to Clean**:
+   - Click the "Select a Folder" button.
+   - Navigate to and select the folder containing HTML files to be cleaned.
 
-- Remove `<script>` tags from the header.
-- Remove `<nav></nav>` tags from the HTML content.
-- Remove a user-specified string from `<link>` and `<img>` tags.
-- Copy files from subfolders to a central `_files` folder in the root directory.
-- Optionally delete subfolders after successful copying (you will be prompted to confirm).
+2. **Apply Cleaning Options**:
+   - You can select various cleaning options, including:
+       - Removing links to external sources.
+       - Removing GitHub links (if checked, the user will be prompted before deletion).
+       - Skipping subfolders (if checked, you will be prompted before deletion).
+
+3. **Convert to PDF (Optional)**:
+   - Provide input and output folders for PDF conversion.
+   - Click the "Browse" buttons to select these folders.
+   - Click the "Convert to PDF" button to start the conversion process. The program will generate PDF files from the cleaned HTML content.
+
+## Running the Program
+
+- Execute the Python script to launch the GUI and start using the tool.
+- The program will guide you through the process, and you can view the progress and results in the GUI itself.
+
 
 3. You will see the processed subfolders and any subfolders that were deleted (if applicable) in the program's output.
 
--- src/app.py
+++ src/app.py
@@ -6,19 +6,19 @@ import os
 import shutil
 from bs4 import BeautifulSoup
 import re
+import pdfkit
 
 # Create the root window
 root = tk.Tk()
 root.title('Choose Folder')
 root.resizable(False,False)
 
-window_width = 500
-window_height = 250
+window_width = 600
+window_height = 400
 
 def center_window(root, width, height):
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
-    
     x = (screen_width - width) // 2
     y = (screen_height - height) // 2
     
@@ -31,6 +31,109 @@ def remove_head_scripts(html_content):
         scripts = head.find_all('script')
         for script in scripts:
             script.extract()  # Remove the script tag
+            
+    print(f"Removed scripts from header: {len(scripts)} script tags")
+    return str(soup)
+
+def remove_head_links(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    head = soup.find('head')
+    if head:
+        scripts = head.find_all('link')
+        for script in scripts:
+            script.extract()  # Remove the script tag
+            
+    print(f"Removed head links: {len(scripts)} link tags")
+    
+    return str(soup)
+
+def remove_head_meta_tags(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    head = soup.find('head')
+    if head:
+        scripts = head.find_all('meta')
+        for script in scripts:
+            script.extract()  # Remove the script tag
+            
+    meta_tag = '<meta content="text/html; charset=UTF-8" http-equiv="Content-Type"/>'
+    
+    meta_element = BeautifulSoup(meta_tag, 'html.parser')
+
+    head.insert(position=0, new_child=meta_element)
+    
+    print(f"Removed head meta tags: {len(scripts)} meta tags. (Keeping meta tag with charset for better pdf converstion)")
+    
+    return str(soup)
+
+def remove_style_tags_with_data_emotion(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    style_tags = soup.find_all('style', attrs={'data-emotion': True})
+    
+    for style_tag in style_tags:
+        style_tag.extract()  # Remove the style tag from the HTML content
+
+    print(f"Removed: {len(style_tags)} style tags with data-emotion")
+
+    return str(soup)
+
+#adding a new div after body to prevent wkhtmltopdf from createing blank pages.
+def add_extra_div_after_body(html_content):
+    soup = BeautifulSoup(html_content)    
+    body = soup.find('body')
+    
+    if body:
+        div = '<div></div>'
+        div_element = BeautifulSoup(div, 'html.parser')    
+        body.insert_after(div_element)
+        body.insert(position=0, new_child=div_element)
+        
+    return str(soup) 
+  
+def remove_attributes_from_div_section_main_tags(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    
+    # Remove attributes from div tags
+    div_tags = soup.find_all('div')
+    for div_tag in div_tags:
+        for attribute in list(div_tag.attrs):
+            del div_tag[attribute]
+    
+    # Remove attributes from section tags
+    section_tags = soup.find_all('section')
+    for section_tag in section_tags:
+        for attribute in list(section_tag.attrs):
+            del section_tag[attribute]
+    
+    # Remove attributes from main tags
+    main_tags = soup.find_all('main')
+    for main_tag in main_tags:
+        for attribute in list(main_tag.attrs):
+            del main_tag[attribute]
+
+    print(f"Removed attributes from divs, sections, and main tags: {len(div_tags)}, {len(section_tags)},  {len(main_tags)}")
+
+    return str(soup)
+
+def remove_iframe_tags(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    iframes = soup.find_all('iframe')
+    if iframes:
+        for frame in iframes:
+            frame.extract()
+            
+    print(f"Changed: {len(iframes)} iframes")
+    
+    return str(soup)
+
+def remove_hidden_button(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Remove button tags
+    button_tags = soup.find_all('button')
+    for button_tag in button_tags:
+        button_tag.extract()  # Remove the button element
+
+    print(f"Buttons removed: {len(button_tags)}")
     return str(soup)
 
 def remove_nav_and_div(html_content):
@@ -57,6 +160,8 @@ def remove_string_from_links(html_content, string_to_remove):
     for link in links:
         if string_to_remove in link['href']:
             link['href'] = link['href'].replace(string_to_remove, '')
+    print(f"Changed: {len(links)} links")
+    
     return str(soup)
 
 def remove_string_from_images(html_content, string_to_remove):
@@ -65,6 +170,27 @@ def remove_string_from_images(html_content, string_to_remove):
     for image in images:
         if string_to_remove in image.get('src', ''):
             image['src'] = image.get('src', '').replace(string_to_remove, '')
+    
+    print(f"Changed: {len(images)} imgs")
+    
+    return str(soup)
+
+def remove_string_from_script_tags(html_content, string_to_remove):
+
+    # Parse the HTML content with BeautifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Find all script tags
+    script_tags = soup.find_all('script')
+
+    for script_tag in script_tags:
+        src = script_tag.get('src', '')
+        if string_to_remove in src:
+            # Replace the old src with the new src
+            script_tag['src'] = src.replace(string_to_remove, '')
+
+    print(f"Changed: {len(script_tags)} script tags")
+    
     return str(soup)
 
 def remove_url_from_anchor_tags(html_content):
@@ -82,6 +208,8 @@ def remove_url_from_anchor_tags(html_content):
             continue  # Keep the link
         else:
             link['href'] = ''
+    
+    print(f"Removed: {len(links)} links")
     return str(soup)
 
 def process_files(folder):
@@ -105,8 +233,16 @@ def process_files(folder):
 
         modified_content = remove_head_scripts(html_content)
         modified_content = remove_nav_and_div(modified_content)
+        modified_content = remove_head_links(modified_content)
+        modified_content = remove_head_meta_tags(modified_content)
         modified_content = remove_string_from_links(modified_content, base_filename)
         modified_content = remove_string_from_images(modified_content, base_filename)
+        modified_content = remove_string_from_script_tags(modified_content, base_filename)
+        modified_content = remove_iframe_tags(modified_content)
+        modified_content = add_extra_div_after_body(modified_content)
+        modified_content = remove_attributes_from_div_section_main_tags(modified_content)
+        modified_content = remove_hidden_button(modified_content)
+        # modified_content = remove_style_tags_with_data_emotion(modified_content)
         
         if remove_urls_var.get():
             modified_content = remove_url_from_anchor_tags(modified_content)
@@ -144,8 +280,62 @@ def confirm_selection(folder):
     confirm_selection = askokcancel("Confirmation", f"Confirm path: '{folder}'")
     return confirm_selection
     
+def convert_to_pdf(input_folder, output_folder, wkhtmltopdf_path):
+    options = {
+        'page-size': 'Letter',
+        'margin-top': '10mm',
+        'margin-right': '10mm',
+        'margin-bottom': '10mm',
+        'margin-left': '10mm',
+        '--debug-javascript': None,  
+        '--enable-local-file-access': None,
+        '--disable-external-links': None,
+        '--zoom': '.9',
+        '--minimum-font-size': '20',
+        '--quiet': None,
+    }
 
+    # Specify the configuration with the 'wkhtmltopdf' path
+    configuration = pdfkit.configuration(wkhtmltopdf=wkhtmltopdf_path)
+
+    html_files = [f for f in os.listdir(input_folder) if f.endswith(".html")]
+    for html_file in html_files:
+        input_path = os.path.join(input_folder, html_file)
+        output_file = os.path.splitext(html_file)[0] + '.pdf'
+        output_path = os.path.join(output_folder, output_file)
+
+        try:
+            pdfkit.from_file(input_path, output_path, configuration=configuration, options=options)
+        except Exception as e:
+            e = e.with_traceback(e.__traceback__)
+          
+            print(f"Error converting {input_path} to PDF: {repr(e)}")
+  
+    showinfo(
+        title='Conversion Complete',           
+        message=f'{len(html_files)} HTML files converted to PDF.'
+    )
+
+def select_input_folder():
+    global selected_input_folder
+    global asset_path
+    selected_input_folder = fd.askdirectory(title="Select Input Folder", initialdir="/")   
+    # asset_path = fd.askdirectory(title="Select Input Folder", initialdir="/")   
+    if selected_input_folder:
+        input_folder_entry.delete(0, tk.END)
+        input_folder_entry.insert(0, selected_input_folder)
+
+def select_output_folder():
+    global selected_output_folder
+    selected_output_folder = fd.askdirectory(title="Select Output Folder", initialdir="/")
+    if selected_output_folder:
+        output_folder_entry.delete(0, tk.END)
+        output_folder_entry.insert(0, selected_output_folder)
+        
 def select_folder():
+    global selected_input_folder
+    global selected_output_folder
+    
     folder = fd.askdirectory(
         title='Select a Folder',
         initialdir='/'
@@ -153,7 +343,16 @@ def select_folder():
     if folder:
         folder_path_label.config(text=f"Chosen Folder: {folder}")  # Update the label with the chosen folder path
 
-    if folder and confirm_selection(folder):
+    if folder and confirm_selection(folder):   
+        selected_input_folder = folder
+        selected_output_folder = folder
+        
+        output_folder_entry.delete(0, tk.END)
+        input_folder_entry.delete(0, tk.END)
+        
+        
+        output_folder_entry.insert(0, selected_output_folder)
+        input_folder_entry.insert(0, selected_input_folder)
         
         process_files(folder)
         destination_folder = os.path.join(folder, "_files")
@@ -164,16 +363,17 @@ def select_folder():
                     files_path = os.path.join(folder, subfolder)
                     files = get_files(files_path)
                     copy_files(files, files_path ,destination_folder)
-                    print(f"Subfolder: {subfolder}")
+                    
+                    print(f"Subfolder: {subfolder} containing {len(files)} copied.")
                     
                      # Prompt the user to delete the subfolder
-                    delete_subfolder_with_confirmation(files_path)
-                  
+                    delete_subfolder_with_confirmation(files_path)          
         else:
             showinfo(
                 title='Nothing found',
                 message=f'No subfolders in directory.'
             )
+            
     showinfo(
             title='Processing Complete',
             message=f'Process complete.'
@@ -183,6 +383,12 @@ skip_subfolders_var = tk.BooleanVar(value=False)  # Default value is False
 remove_urls_var = tk.BooleanVar(value=True)  # Default value is False
 skip_github_urls_var = tk.BooleanVar(value=True)  # Default value is False
 
+open_button = ttk.Button(
+    root,
+    text='Select a Folder',
+    command=select_folder
+)
+
 check_can_remove_external_links = ttk.Checkbutton(
     root,
     text='Remove links to external sources?',
@@ -191,35 +397,63 @@ check_can_remove_external_links = ttk.Checkbutton(
 
 check_can_remove_github_links = ttk.Checkbutton(
     root,
-    text='Skip links to GitHub?',
+    text='Skip links to GitHub?: only matters if remove links is checked.',
     variable=skip_github_urls_var,
 )
 
-open_button = ttk.Button(
-    root,
-    text='Select a Folder',
-    command=select_folder
-)
-
-
 skip_subfolders_checkbox = ttk.Checkbutton(
     root,
-    text='Skip Subfolders',
+    text='Skip Subfolders: If checked, you will be prompted before deletion.',
     variable=skip_subfolders_var,
+)
+convert_button = ttk.Button(
+    root,
+    text='Convert to PDF',
+    command=lambda: convert_to_pdf(selected_input_folder, selected_output_folder, wkhtmltopdf_path="/usr/local/bin/wkhtmltopdf")
 )
 
 
-check_can_remove_external_links.grid(row=0, column=0, sticky="w", padx=10)
-check_can_remove_github_links.grid(row=1, column=0, sticky="w", padx=10)
-skip_subfolders_checkbox.grid(row=2, column=0, sticky="w", padx=10)
+input_folder_entry = ttk.Entry(root)
+output_folder_entry = ttk.Entry(root)
+
+
+tk.Label(root, text="Step: 1 choose folder to clean").grid(row=1, sticky="w")
 
 # Create a Label widget to display the chosen folder path
 folder_path_label = ttk.Label(root, text="Chosen Folder: None")
-folder_path_label.grid(row=9, column=0, columnspan=5, padx=10)
+ 
+folder_path_label.grid(row=2, column=0, columnspan=10, sticky="w")
+open_button.grid(row=3, column=1, columnspan=1, sticky="w")
 
-tk.Label(root, text="").grid(row=3)
+check_can_remove_external_links.grid(row=4, column=0, sticky="w", padx=10)
+check_can_remove_github_links.grid(row=5, column=0, sticky="w", padx=10, columnspan=5)
 
-open_button.grid(columnspan=4)
+skip_subfolders_checkbox.grid(row=6, column=0, sticky="w", padx=10, columnspan=10)
+
+
+tk.Label(root, text="Step: 2 make pdf. (optional) ").grid(row=12, sticky="w")
+
+
+input_folder_label = ttk.Label(root, text="Input Folder:")
+output_folder_label = ttk.Label(root, text="Output Folder:")
+
+
+input_folder_label = ttk.Label(root, text="Input Folder:")
+input_folder_label.grid(row=14, column=0)
+
+output_folder_label = ttk.Label(root, text="Output Folder:")
+output_folder_label.grid(row=15, column=0)
+
+
+input_folder_button = ttk.Button(root, text="Browse", command=select_input_folder)
+input_folder_button.grid(row=14, column=2, sticky="w")
+input_folder_entry.grid(row=14, column=1)
+
+output_folder_button = ttk.Button(root, text="Browse", command=select_output_folder)
+output_folder_button.grid(row=15, column=2, sticky="w")
+output_folder_entry.grid(row=15, column=1)
+
+convert_button.grid(row=16, column=1, sticky="w")
 
 center_window(root, window_width, window_height)
 
